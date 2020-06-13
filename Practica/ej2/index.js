b353467de5a3e73df2ff915af4e8d9d610421efe
Gustavo Tiseira <gustavotiseira@gmail.com>
2020-06-13 16:18:04 -0300

    v-on methods

--- Practica/ej2/index.js
+++ Practica/ej2/index.js
@@ -2,6 +2,21 @@ var app = new Vue({
   el: '#app',
   data: {
     seen: true,
+    help: 'Fecha ' + new Date().toDateString(),
+    showOtherMessage: false,
+    value: '',
+    todos: [
+      { text: '1º Primero' },
+      { text: '2º Segundo' },
+      { text: '3º Tercero' },
+      { text: '4º Cuarto' },
+      { text: '5º Quinto' },
+    ],
+  },
+  methods: {
+    alertConsole: () => {
+      console.log('Hey Alert Please');
+    },
   },
 });
 
